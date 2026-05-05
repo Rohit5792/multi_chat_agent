fdd99c4886894a2978448bec799790e37194c7e1
Rohit5792 <rohitaher604@gmail.com>
2026-05-05 18:19:28 +0530

    env

--- app/config.py
+++ app/config.py
@@ -5,11 +5,10 @@ load_dotenv()
 
 OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-LLM_MODEL = os.getenv("LLM_MODEL")
+LLM_MODEL = os.getenv("LLM_MODEL") or "openai/gpt-oss-120b:free"
 
 DB_URL = os.getenv("DB_URL")
 CHROMA_DIR = os.getenv("CHROMA_DIR", "./chroma_db")
 GOOGLE_API_KEY    = os.getenv("GOOGLE_API_KEY")
 GOOGLE_EMBED_MODEL = os.getenv("GOOGLE_EMBED_MODEL")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-LLM_MODEL = os.getenv("LLM_MODEL")
